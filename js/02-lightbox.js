54193bbee2ebcb8364f3c7c886c2b1e3797ab9b4
erbium6 <86596363+erbium6@users.noreply.github.com>
2021-12-16 13:48:53 +0200

    finish HW 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRefs = document.querySelector(".gallery");
+
+const fullGallery = galleryItems
+    .map(
+        item =>
+      `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}"
+             alt="${item.description}"/>
+       </a>`
+  )
+    .join("");
+
+galleryRefs.insertAdjacentHTML("beforeend", fullGallery);
+
+let image = '';
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
